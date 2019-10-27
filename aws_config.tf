61e2b8ce4d14ccb11bba9f1d7fa46150070fb364
Sean P. Kane <skane@newrelic.com>
2019-10-27 10:40:53 -0700

    tf12 boolean for count fixes

--- aws_config.tf
+++ aws_config.tf
@@ -1,5 +1,5 @@
 resource "aws_config_configuration_recorder" "aws_config_recorder" {
-  count = var.enable_aws_config
+  count = ${var.enable_aws_config ? 1 : 0}"
   name  = "terraform_config_recorder"
 
   recording_group {
@@ -11,14 +11,14 @@ resource "aws_config_configuration_recorder" "aws_config_recorder" {
 }
 
 resource "aws_config_configuration_recorder_status" "aws_config_recorder_status" {
-  count      = var.enable_aws_config
+  count      = ${var.enable_aws_config ? 1 : 0}"
   name       = aws_config_configuration_recorder.aws_config_recorder[count.index].name
   is_enabled = true
   depends_on = ["aws_config_delivery_channel.aws_config_delivery_channel"]
 }
 
 resource "aws_s3_bucket" "aws_config_configuration_bucket" {
-  count  = var.enable_aws_config
+  count  = ${var.enable_aws_config ? 1 : 0}"
   bucket = "${data.aws_caller_identity.current.account_id}-terraform-aws-config-bucket"
 
   tags = "${merge(map("Project","shared"),
@@ -27,12 +27,12 @@ resource "aws_s3_bucket" "aws_config_configuration_bucket" {
 }
 
 resource "aws_sns_topic" "aws_config_updates_topic" {
-  count = var.enable_aws_config
+  count = ${var.enable_aws_config ? 1 : 0}"
   name  = "${data.aws_caller_identity.current.account_id}-terraform-aws-config-updates"
 }
 
 resource "aws_config_delivery_channel" "aws_config_delivery_channel" {
-  count          = var.enable_aws_config
+  count          = ${var.enable_aws_config ? 1 : 0}"
   name           = "terraform_aws_config_delivery_channel"
   s3_bucket_name = aws_s3_bucket.aws_config_configuration_bucket[count.index].bucket
   sns_topic_arn  = aws_sns_topic.aws_config_updates_topic[count.index].arn
@@ -44,20 +44,20 @@ data "template_file" "aws_config_iam_assume_role_policy_document" {
 }
 
 resource "aws_iam_role" "aws_config_iam_role" {
-  count              = var.enable_aws_config
+  count              = ${var.enable_aws_config ? 1 : 0}"
   name               = "terraform-awsconfig-role"
   assume_role_policy = data.template_file.aws_config_iam_assume_role_policy_document.rendered
 }
 
 resource "aws_iam_role_policy_attachment" "aws_config_iam_policy_attachment" {
-  count      = var.enable_aws_config
+  count      = ${var.enable_aws_config ? 1 : 0}"
   role       = aws_iam_role.aws_config_iam_role[count.index].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
 data "template_file" "aws_config_iam_policy_document" {
   template = "${file("${path.module}/policies/aws_config_policy.tpl")}"
-  count    = var.enable_aws_config
+  count    = ${var.enable_aws_config ? 1 : 0}"
 
   vars = {
     sns_topic_arn = aws_sns_topic.aws_config_updates_topic[count.index].arn
@@ -66,7 +66,7 @@ data "template_file" "aws_config_iam_policy_document" {
 }
 
 resource "aws_iam_role_policy" "aws_config_iam_policy" {
-  count  = var.enable_aws_config
+  count  = ${var.enable_aws_config ? 1 : 0}"
   name   = "terraform-awsconfig-policy"
   role   = aws_iam_role.aws_config_iam_role[count.index].id
   policy = data.template_file.aws_config_iam_policy_document[count.index].rendered
